3a9a90555acd56fe8ad249f33851eb6613abcf40
Žan Rode <zanr.comedy@gmail.com>
2021-08-16 18:12:02 +0200

    States and calls work now. TODO: Style the weather display

--- src/components/Weatherdisplay.jsx
+++ src/components/Weatherdisplay.jsx
@@ -1,26 +1,20 @@
 import React from 'react';
 
-// import { useEffect } from "react"
-
 const Weatherdisplay = ({ value }) => {
-    const API_KEY = process.env.REACT_APP_API_KEY
-    async function getData() {
-        try {
-            const data = await fetch(`api.openweathermap.org/data/2.5/weather?q=${value}&appid=${API_KEY}`)
-            const parsed = await data.json()
-            return parsed
-        } catch(error) {
-            return alert(`There has been an error: ${error}`)
-        }
-    }
-
-    console.log(getData())
-
+    try {
+    let temp = value.data.main.temp
+    let city = value.data.name
     return(
         <div className='weatherdisplay'>
-            <p>{}</p>
+            <p>{`Current temperature in ${city} is ${temp} degrees Celsius!`}</p>
         </div>
-    )
+    )} catch(err) {
+        return(
+            <div className="weatherdisplay notfound">
+                <p className="notfound-text">City not found!</p>
+            </div>
+        )
+    }
 }
 
 export default Weatherdisplay
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, {useEffect, useState } from 'react';
 import Title from '../components/Title'
 import Weatherdisplay from '../components/Weatherdisplay'
 import Footer from '../components/Footer'
@@ -7,17 +7,30 @@ import './App.css';
 
 const App = () => {
     const [input, setInput] = useState('')
+    const [weatherData, setWeatherData] = useState({})
 
+    useEffect(() => {
+        const API_KEY = process.env.REACT_APP_WEATHER_API_KEY
+
+        if (!input) {
+            setInput('London');
+        }
+
+        fetch(`http://api.openweathermap.org/data/2.5/weather?q=${input}&units=metric&lang=en&appid=${API_KEY}`)
+        .then(response => response.json())
+        .then(data => setWeatherData({data}))
+        .catch(err => setWeatherData(false))
+    }, [input])
+    
+    //! function that handles a change in the input and sets the state to the value
     function handleChange(event) {
-		setInput(event.target.value)
-	}
-
-    console.log(input)
+        setInput(event.target.value);
+    }
 
     return(
         <main className='bg-light-blue'>
             <Title onChange={handleChange} value={input}/>
-            <Weatherdisplay value={input}/>
+            <Weatherdisplay value={weatherData}/>
             <Footer />
         </main>
     )
